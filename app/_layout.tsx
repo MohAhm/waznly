a62f3bb701f01a83f004d178d11986f11f8216ac
Mohamed Ali <a.mohamed.cali@gmail.com>
2025-06-16 21:58:41 +0200

    Set up bottom tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,38 @@
-import { Stack } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Octicons from "@expo/vector-icons/Octicons";
+import { Tabs } from "expo-router";
+import { theme } from "../them";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorDefault }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ size, color }) => (
+            <Entypo name="user" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="graph"
+        options={{
+          title: "Graph",
+          tabBarIcon: ({ size, color }) => (
+            <Octicons name="graph" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          tabBarIcon: ({ size, color }) => (
+            <Octicons name="clock" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
---
+++ them.ts
@@ -0,0 +1,4 @@
+export const theme = {
+  colorDefault: "#5E85F8",
+  colorWhite: "#FFF",
+};
